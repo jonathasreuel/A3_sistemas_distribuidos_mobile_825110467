1ba71412a6d30c897549cc2219c5679a1542ceee
Jonathas Reuel <jowrsl@gmail.com>
2025-06-11 23:48:13 -0300

    chore: Adding routes

---
+++ app/routes/carroRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/carroController');
+
+router.get('/', controller.listar);
+router.get('/:id', controller.buscarPorId);
+router.post('/', controller.criar);
+router.put('/:id', controller.atualizar);
+router.patch('/:id', controller.atualizarParcial);
+router.delete('/:id', controller.remover);
+
+module.exports = router;
